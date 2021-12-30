4faf40ccf47d6ca4bdf5be4072f43f4ee056116d
Ryan Macdonald <hi@ryanmacdonald.ca>
2021-12-30 17:45:11 -0500

    Improve console.log in examples

--- examples/getting-started/main.js
+++ examples/getting-started/main.js
@@ -9,25 +9,30 @@ const { Money, exchange } = new Mint({
 
 exchange.addRate(USD, CAD, 1.26);
 exchange.addRate(CAD, USD, 0.74);
-console.log('International rates setup');
 
 const wallet = Money();
 console.log(`My wallet has ${wallet.toLocaleString()}`);
 
 const payment1 = Money(1000);
 wallet.add(payment1);
-console.log(`Received payment of ${payment1.toLocaleString()}`);
+console.log(`+ Received payment of ${payment1.toLocaleString()}`);
 console.log(`My wallet has ${wallet.toLocaleString()}`);
 
 const payment2 = Money(2000, CAD);
 wallet.add(payment2);
-console.log(`Received payment of ${payment2.toLocaleString()} in CAD`);
+console.log(`+ Received payment of ${payment2.toLocaleString()}`);
+console.log(
+  `Coverting to USD: ${payment2} -> ${exchange.exchangeWith(payment2, USD)}`
+);
 console.log(`My wallet has ${wallet.toLocaleString()}`);
 
 const spending = Money(1000, CAD);
-console.log(`Spending ${spending}`);
+console.log(`- Spending ${spending.toLocaleString()}`);
 console.log(
-  `Coverting to USD. ${spending} -> ${exchange.exchangeWith(spending, USD)}`
+  `Coverting to USD: ${spending.toLocaleString()} -> ${exchange.exchangeWith(
+    spending,
+    USD
+  )}`
 );
 wallet.subtract(Money(1000, CAD));
 console.log(`My wallet has ${wallet.toLocaleString()}`);
